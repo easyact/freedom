898a819aa96e502888bfafbf41eb30780c36566e
zhaolei <zhaolei@easyact.cn>
2020-01-27 18:34:15 +0800

    * bu ep

--- src/main/scala/cn/easyact/fin/controllers/hello/BuHandler.scala
+++ src/main/scala/cn/easyact/fin/controllers/hello/BuHandler.scala
@@ -1,19 +1,20 @@
 package cn.easyact.fin.controllers.hello
 
 import java.time.LocalDate
+import java.util
 import java.util.UUID
 
 import cn.easyact.fin.manager.BudgetUnitCommands._
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import org.apache.logging.log4j.{LogManager, Logger}
 
-class BuHandler extends RequestHandler[Map[String, String], BU] {
+class BuHandler extends RequestHandler[util.Map[String, String], BU] {
 
   val logger: Logger = LogManager.getLogger(getClass)
 
-  def handleRequest(in: Map[String, String], context: Context): BU = {
+  def handleRequest(in: util.Map[String, String], context: Context): BU = {
     logger.info(s"Received a request: $in")
-    val name = in("name")
+    val name = in get "name"
     val id = UUID.fromString(name).toString
     register(id, name, Some(LocalDate now()))
     new BU(id, name)
