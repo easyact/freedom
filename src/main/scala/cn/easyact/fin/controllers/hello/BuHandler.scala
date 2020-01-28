2d70e2784627c71d3ffdc279efe1bea0daef982e
zhaolei <zhaolei@easyact.cn>
2020-01-28 10:24:26 +0800

    * deserialize exception

--- src/main/scala/cn/easyact/fin/controllers/hello/BuHandler.scala
+++ src/main/scala/cn/easyact/fin/controllers/hello/BuHandler.scala
@@ -8,13 +8,13 @@ import cn.easyact.fin.manager.BudgetUnitCommands._
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import org.apache.logging.log4j.{LogManager, Logger}
 
-class BuHandler extends RequestHandler[JMap[String, String], BU] {
+class BuHandler extends RequestHandler[JMap[String, Object], BU] {
 
   val logger: Logger = LogManager.getLogger(getClass)
 
-  def handleRequest(in: JMap[String, String], context: Context): BU = {
+  def handleRequest(in: JMap[String, Object], context: Context): BU = {
     logger.info(s"Received a request: $in")
-    val name = in get "name"
+    val name = in.get("name").asInstanceOf[String]
     val id = randomUUID.toString
     register(id, name, Some(LocalDate now()))
     new BU(id, name)
---
+++ src/main/scala/cn/easyact/fin/controllers/hello/BuResponse.scala
@@ -0,0 +1,7 @@
+package cn.easyact.fin.controllers.hello
+
+import scala.beans.BeanProperty
+import java.util.{Map => JMap}
+
+case class BuResponse(@BeanProperty statusCode: Integer, @BeanProperty body: BU,
+                      @BeanProperty headers: JMap[String, String] = JMap.of(), @BeanProperty base64Encoded: Boolean = false)
