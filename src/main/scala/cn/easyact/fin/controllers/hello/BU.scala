020ad16c1c07bf3ef24594a52b7c1ecb5170390a
zhaolei <zhaolei@easyact.cn>
2020-01-28 10:36:01 +0800

    * Execution failed due to configuration error: Malformed Lambda proxy response

--- src/main/scala/cn/easyact/fin/controllers/hello/BU.scala
+++ src/main/scala/cn/easyact/fin/controllers/hello/BU.scala
@@ -2,7 +2,7 @@ package cn.easyact.fin.controllers.hello
 
 import scala.beans.BeanProperty
 
-class BU(@BeanProperty var id: String, @BeanProperty var name: String) {
+case class BU(@BeanProperty var id: String, @BeanProperty var name: String) {
   override def toString: String = s"BU($id, $name)"
 }
 
--- src/main/scala/cn/easyact/fin/controllers/hello/BuHandler.scala
+++ src/main/scala/cn/easyact/fin/controllers/hello/BuHandler.scala
@@ -8,15 +8,15 @@ import cn.easyact.fin.manager.BudgetUnitCommands._
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import org.apache.logging.log4j.{LogManager, Logger}
 
-class BuHandler extends RequestHandler[JMap[String, Object], BU] {
+class BuHandler extends RequestHandler[JMap[String, Object], BuResponse] {
 
   val logger: Logger = LogManager.getLogger(getClass)
 
-  def handleRequest(in: JMap[String, Object], context: Context): BU = {
+  def handleRequest(in: JMap[String, Object], context: Context): BuResponse = {
     logger.info(s"Received a request: $in")
     val name = in.get("name").asInstanceOf[String]
     val id = randomUUID.toString
     register(id, name, Some(LocalDate now()))
-    new BU(id, name)
+    BuResponse(200, BU(id, name))
   }
 }
