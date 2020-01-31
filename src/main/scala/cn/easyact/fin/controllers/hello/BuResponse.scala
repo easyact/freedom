a00bec19a344db4150276593a575076f0aeceddd
zhaolei <zhaolei@easyact.cn>
2020-01-31 13:02:12 +0800

    * cors

--- src/main/scala/cn/easyact/fin/controllers/hello/BuResponse.scala
+++ src/main/scala/cn/easyact/fin/controllers/hello/BuResponse.scala
@@ -3,5 +3,8 @@ package cn.easyact.fin.controllers.hello
 import scala.beans.BeanProperty
 import java.util.{Map => JMap}
 
-case class BuResponse(@BeanProperty statusCode: Integer, @BeanProperty body: String,
-                      @BeanProperty headers: JMap[String, String] = JMap.of(), @BeanProperty base64Encoded: Boolean = false)
+case class BuResponse(@BeanProperty statusCode: Integer,
+                      @BeanProperty body: String,
+                      @BeanProperty headers: JMap[String, _] = JMap.of("Access-Control-Allow-Origin", "*", "Access-Control-Allow-Credentials", true),
+                      @BeanProperty base64Encoded: Boolean = false) {
+}
