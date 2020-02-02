91ec7ce7376e8aa4b6f241b7ff9ee98adab20301
zhaolei <zhaolei@easyact.cn>
2020-02-02 10:36:18 +0800

    * reproduce

--- src/test/scala/cn/easyact/fin/manager/ScalaStepdefs.scala
+++ src/test/scala/cn/easyact/fin/manager/ScalaStepdefs.scala
@@ -26,7 +26,8 @@ class ScalaStepdefs extends BudgetUnitCommands(MockTimeService) {
   import MemReadService._
 
   private var result: Error \/ List[MonthlyForecast] = _
-  private var script: Free[Event, BudgetUnit] = _
+  //  private var script: Free[Event, BudgetUnit] = _
+  private var no: String = _
 
   @Before(order = 10)
   def setInterpreter(): Unit = {
@@ -36,16 +37,18 @@ class ScalaStepdefs extends BudgetUnitCommands(MockTimeService) {
   }
 
   @假设("{string}开了户") def 张三在号开了户(name: String): Unit = {
-    script = register(name, name, Some(today), now)
+    val script = register(name, name, Some(today), now)
+    perform(script)
   }
 
   @假设("月收入:") def 月收入(incomes: util.List[Income]): Unit = {
-    script = incomes.asScala.foldLeft(script)((s, income) => s flatMap (bu =>
-      addItem(bu.no, income, now)))
+    val script = incomes.asScala.map(addItem(no, _, now)).reduceLeft((s, command) => s flatMap (_ => command))
+    perform(script)
   }
 
-  @假设("月支出:") def 月支出(incomes: util.List[Expense]): Unit = {
-    script = incomes.asScala.foldLeft(script)((s, income) => s >>= (bu => addItem(bu.no, income)))
+  @假设("月支出:") def 月支出(expenses: util.List[Expense]): Unit = {
+    val script = expenses.asScala.map(addItem(no, _)).reduce((s, income) => s >>= (_ => income))
+    perform(script)
   }
 
   @假设("当前月为{string}") def 当前月为(string: String): Unit = {
@@ -53,15 +56,16 @@ class ScalaStepdefs extends BudgetUnitCommands(MockTimeService) {
   }
 
   @假设("当前存款为{amount}") def 当前存款为(amount: Amount): Unit = {
-    script = script >>= (bu => save(bu.no, amount))
+    perform(save(no, amount))
   }
 
 
   @当("拉{int}个月的预测") def 拉个月的预测(numberOfMonths: Int): Unit = {
-    val task = MemInterpreter(script)
-    val account = task.unsafePerformSync
-    result = forecast(account.no, numberOfMonths)
+    //    val account: BudgetUnit = perform(script)
+    result = forecast(no, numberOfMonths)
   }
+
+  private def perform(script: Command[BudgetUnit]) = MemInterpreter(script).unsafePerformSync
 
   @那么("生成预测:") def 生成预测(forecastList: util.List[MonthlyForecast]): Unit = {
     result.leftMap(assertThat(_).isNullOrEmpty())
